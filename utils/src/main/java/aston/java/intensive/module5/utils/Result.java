6870488acb0bb3291295f6e63810bbf7c3571045
kasnil <kasnil@outlook.ie>
2026-01-22 10:26:18 +0300

    feat: Реализовать валидатор [#68]

--- utils/src/main/java/aston/java/intensive/module5/utils/Result.java
+++ utils/src/main/java/aston/java/intensive/module5/utils/Result.java
@@ -2,12 +2,14 @@ package aston.java.intensive.module5.utils;
 
 import aston.java.intensive.module5.utils.guard.Ensure;
 
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-public class Result<T> extends EmptyResult {
+public class Result<T> extends ResultEmpty {
     private final T value;
 
-    public Result(T value, Exception exception) {
+    public Result(T value, RuntimeException exception) {
         super(exception);
         this.value = value;
     }
@@ -31,11 +33,56 @@ public class Result<T> extends EmptyResult {
         }
     }
 
+    public T orElseThrow() {
+        if (value == null) {
+            throw this.exception;
+        }
+        return value;
+    }
+
+    public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X {
+        if (value != null) {
+            return value;
+        } else {
+            throw exceptionSupplier.get();
+        }
+    }
+
+    public T orElse(T other) {
+        return value != null ? value : other;
+    }
+
+    public T orElseGet(Supplier<? extends T> supplier) {
+        return value != null ? value : supplier.get();
+    }
+
     public static <T> Result<T> ok(T value) {
         return new Result<>(value, null);
     }
 
-    public static <T> Result<T> err(Exception exception) {
+    public static <T> Result<T> err(RuntimeException exception) {
         return new Result<>(null, exception);
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        return obj instanceof Result<?> other
+                && Objects.equals(exception, other.exception)
+                && Objects.equals(value, other.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(exception, value);
+    }
+
+    @Override
+    public String toString() {
+        return value != null
+                ? ("Result[" + value + "]")
+                : ("Result[" + exception + "]");
     }
 }
