e7e0cef27cb5a052f6b4f5180b2bf7568d35d5ec
kasnil <kasnil@outlook.ie>
2026-01-24 17:18:19 +0300

    feat: Реализовать сервис работы с вводом/выводом [#82]

---
+++ infrastructure/src/main/java/aston/java/intensive/module5/infrastructure/io/ConsoleService.java
@@ -0,0 +1,30 @@
+package aston.java.intensive.module5.infrastructure.io;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ConsoleService implements IOService {
+    private final PrintStream output;
+    private final Scanner input;
+
+    public ConsoleService() {
+        output = System.out;
+        input = new Scanner(System.in);
+    }
+
+    @Override
+    public int readInt() {
+        return Integer.parseInt(input.nextLine());
+    }
+
+    @Override
+    public int readInt(String prompt) {
+        output(prompt);
+        return readInt();
+    }
+
+    @Override
+    public void output(Object message) {
+        output.println(message);
+    }
+}
---
+++ infrastructure/src/main/java/aston/java/intensive/module5/infrastructure/io/IOService.java
@@ -0,0 +1,4 @@
+package aston.java.intensive.module5.infrastructure.io;
+
+public interface IOService extends InputService, OutputService {
+}
