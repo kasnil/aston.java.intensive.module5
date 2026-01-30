b603d9fe7a63141fd7a4f1e06c00b5298b8247e9
LLIASOUBOTINE <semenuk1991311@gmail.com>
2026-01-31 01:05:10 +0300

    refactor: устронение замечаний

--- presentation/src/main/java/aston/java/intensive/module5/presentation/menu/view/MenuItem.java
+++ presentation/src/main/java/aston/java/intensive/module5/presentation/menu/view/MenuItem.java
@@ -2,4 +2,34 @@ package aston.java.intensive.module5.presentation.menu.view;
 
 import aston.java.intensive.module5.utils.menu.models.Resource;
 
-public record MenuItem(String text, Resource resource, boolean enabled) {}
+import java.util.function.BooleanSupplier;
+
+public final class MenuItem {
+
+    private final String text;
+    private final Resource resource;
+    private final BooleanSupplier enabled;
+
+    public MenuItem(String text, Resource resource, BooleanSupplier enabled) {
+        this.text = text;
+        this.resource = resource;
+        this.enabled = enabled;
+    }
+
+    public MenuItem(String text, Resource resource) {
+        this(text, resource, () -> true);
+    }
+
+    public String text() {
+        return text;
+    }
+
+    public Resource resource() {
+        return resource;
+    }
+
+    public boolean enabled() {
+        return enabled.getAsBoolean();
+    }
+}
+
