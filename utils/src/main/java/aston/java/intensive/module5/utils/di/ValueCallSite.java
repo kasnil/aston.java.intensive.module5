a76ab0a022b363f5f16045db81dd62aee25ba86b
kasnil <kasnil@outlook.ie>
2026-02-01 18:08:03 +0300

    feat: Добавить DI в обработку циклов меню

--- utils/src/main/java/aston/java/intensive/module5/utils/di/ValueCallSite.java
+++ utils/src/main/java/aston/java/intensive/module5/utils/di/ValueCallSite.java
@@ -2,10 +2,10 @@ package aston.java.intensive.module5.utils.di;
 
 import aston.java.intensive.module5.utils.guard.Ensure;
 
-public class ConstantCallSite extends CallSite {
+public class ValueCallSite extends CallSite {
     private final Object value;
 
-    public ConstantCallSite(Class<?> serviceClass, Object value) {
+    public ValueCallSite(Class<?> serviceClass, Object value) {
         Ensure.that(value).isNotNull();
 
         super(serviceClass);
