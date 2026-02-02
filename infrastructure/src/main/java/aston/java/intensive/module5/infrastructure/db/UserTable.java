e1b484ac6ffc5796ef77d381363f6e8e4694f00b
kasnil <kasnil@outlook.ie>
2026-02-02 21:02:34 +0300

    refactor: Table -> StoreSet

--- infrastructure/src/main/java/aston/java/intensive/module5/infrastructure/db/UserTable.java
+++ infrastructure/src/main/java/aston/java/intensive/module5/infrastructure/db/UserTable.java
@@ -2,5 +2,5 @@ package aston.java.intensive.module5.infrastructure.db;
 
 import aston.java.intensive.module5.domain.User;
 
-public class UserTable extends Table<User> {
+public class UserTable extends StoreSet<User> {
 }
